08085ffdbfbf494954840102724534821151ba64
Lucero-20350288 <isc20350288@gmail.com>
2023-10-02 20:31:18 -0600

    Añadiendo metodo POST con body

--- index.js
+++ index.js
@@ -1,46 +1,66 @@
 const express = require('express')
 const app = express()
 
-//http://localhost:3000/lucero/123
-app.get('/', (req, res) => {
-    res.json({msg: 'Hola GET'})
+app.use(express.json())
+
+//http://localhost:3000/sugey
+app.get('/', (req, res)=> {
+    res.json({msg: 'Hello GET'})
 })
 
-app.get('/:user/:password', (req, res) => { //Endpoint
-    const {user, password} = req.query
-    if (!user || !password) {
-        res.status(400).json({msg: 'You need to provide <user> and <password> params'})
+    app.get('/:user/:password', (req, res)=> {
+    const {user, password} =  req.params
+    if (user === 'julio' && password === '123'){
+    res.json({msg: 'Inicio de sesión exitoso'})
+    return
     }
-    if(user === 'lucero' && password === '123'){
-        res.json({msg: 'Sign In Succesfully'})
-        return
-    }
-    res.status(404).json({msg: 'Wrong user or password'})
+    res.json({msg: 'Fallo en el usuario o contraseña'})
 })
 
-//http://localhost:3000/login?fullname=Lucero%20Lopez%20Garcia
-//http://localhost:3000/login?user=lucero&password=123
+//http://localhost:3000/login?user=Sugey&password=123
 app.get('/login', (req, res) => {
-    const queries = req.query
-    res.json(queries)
+    const {user, password} = req.query
+    if (!user || !password){
+        res.json({msg: 'You need to provide <user> and <password> params'})
+    }
+    
+if(user === 'Lucero' && password === '123'){
+    res.json({msg: 'Sing In Succesfull'})
+    return
+}
+res.status(404).json({msg: 'wrong user or password'})
 })
 
-app.post('/', (req, res) => {
-    res.json({msg: 'Hola POST'})
+app.post('/login', (req, res) => {
+    const {user, password} = req.body
+    if (!user || !password){
+        res.json({msg: 'You need to provide <user> and <password> params'})
+    }
+    
+if(user === 'Lucero' && password === '123'){
+    res.json({msg: 'Sing In succesfull'})
+    return
+}
+res.status(404).json({msg: 'wrong user or password'})
 })
 
-app.put('/', (req, res) => {
-    res.json({msg: 'Hola PUT'})
-})
-app.patch('/', (req, res) => {
-    res.json({msg: 'Hola PATCH'})
-})
-app.delete('/', (req, res) => {
-    res.json({msg: 'Hola DELETE'})
+app.post('/',(req, res)=>{
+    res.json({msg:'Hello POST'})
 })
 
-//http://localhost:3000
+app.put('/',(req, res)=>{
+    res.json({msg:'Hello PUT'})
+})
 
-app.listen(3000, () => {
+app.patch('/',(req, res)=>{
+    res.json({msg:'Hello PATCH'})
+})
+
+app.delete('/',(req, res)=>{
+    res.json({msg:'Hello DELETE'})
+
+})
+
+app.listen(3000,()=>{
     console.log('listening on port 3000')
 })
